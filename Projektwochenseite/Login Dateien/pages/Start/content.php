1f838a848af3b0b393edfe2948f9d5b7275cd88d
TheRiotSnake <Beckermumi@web.de>
2017-02-14 11:10:11 +0100

    Update content.php

--- Projektwochenseite/Login Dateien/pages/Start/content.php
+++ Projektwochenseite/Login Dateien/pages/Start/content.php
@@ -1,3 +1,12 @@
 <section>
-	<h2 style="padding-left:1em">Willkommen, <?php echo $_SESSION['name'];?>!</h2>
+	<h1 class="center" style="padding-left:1em">Willkommen, <?php echo $_SESSION['name'];?>!</h1>
+	<div class="container">
+	<p>
+		Hier kannst du dich für Projekte eintragen. Bedenke, dass es eine 1., 2. und 3. Wahl gibt.
+	</p>
+	 <img 
+	      style="
+		     
+     "src="pic1" alt="Bild kann nicht geladen werden">
+	</div>
 </section>
